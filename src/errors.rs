db0847f221117259debbbe33dcdb076b562db17a
Adam <adam.robert.motta@gmail.com>
2026-03-12 11:21:07 +0100

    adding custom Error type

--- src/errors.rs
+++ src/errors.rs
@@ -1,1 +1,25 @@
 // Types d'erreurs personnalisés (thiserror)
+
+use std::path::PathBuf;
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum BackupError {
+
+    //Erreurs métier : 
+    #[error("Invalid sources : {0}")]
+    SourceNotFound(PathBuf),
+
+    #[error("Invalid destination path")]
+    InvalidDestination(PathBuf),
+
+    #[error("Invalid Configuration: {0}")]
+    InvalidConfig(String),
+
+    //Erreurs techniques :
+    #[error("I/O error: {0}")]
+    Io(#[from] std::io::Error),
+}
+
+pub type Result<T> = std::result::Result<T, BackupError>;
